714d01bcbb951c2798963bfd92e62d0c542332fa
Hojin65 <j8960925@naver.com>
2025-03-14 01:55:57 +0000

    test commit

--- 2/iostream.cpp
+++ 2/iostream.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
         int x{};
         std::cout << "Enter a number: ";
         std::cin >> x;
-        std::cout << "You entered" << x << std::endl;
+        std::cout << "You entered " <<  x  << std::endl;
         return 0;
 
 }
