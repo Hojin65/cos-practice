949e4b23f88cfd264947e90a50b55406f6517401
User <22>
2025-03-27 15:01:33 +0000

    new dir

---
+++ alone/5/pointer-1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#define BUFLEN 10
+
+int main(int argc, char *argv[]) {
+    int arr[BUFLEN];
+    int *p;
+    int a, i;
+
+    for (i=0; i<BUFLEN; i++)
+        arr[i] = 10-i;
+
+    a = 5;
+    p =&a;
+
+    cout << "a = " << a << endl;
+    cout << "p = " << p << endl;
+    cout << "&a = " << &a << endl;
+    cout << "*p = " << *p << endl;
+    cout << "arr = " << &arr[0] << endl;
+
+    return 0;
+}
